cf36ea8a31910c78f72eeff5712730dbfcfa6d6e
sjarvela89 <sjarvela89@hotmail.com>
2024-10-21 11:20:31 +0300

    Update Author page.

--- src/app/Author/page.tsx
+++ src/app/Author/page.tsx
@@ -2,21 +2,24 @@ import React from 'react';
 import Link from 'next/link'; // Import Link for navigation
 import Background from '../Components/Background';
 import BusinessCard from '../Components/BusinessCard';
+import styles from './Author.module.css';
 
-const Dog: React.FC = () => {
+const Author: React.FC = () => {
   return (
-    <div className="relative min-h-screen bg-gray-100 flex items-center justify-center overflow-hidden">
+    <div className="relative min-h-screen bg-gray-100 flex flex-col items-center justify-center overflow-hidden">
       <Background />
-      <BusinessCard />
-
-      {/* Home Button */}
-      <div className="absolute top-4 left-4">
-        <Link href="/" className="inline-block bg-[#3182ce] text-white px-4 py-2 rounded-lg font-semibold hover:bg-blue-600 transition-colors">
-          Home
-        </Link>
+      <div className="flex flex-col items-center space-y-8">
+        <div className={styles.homeButtonContainer}>
+          <Link href="/" className="inline-block bg-[#3182ce] text-white px-4 py-2 rounded-lg font-semibold hover:bg-blue-600 transition-colors">
+            Home
+          </Link>
+        </div>
+        <div className="pb-8"> {/* Add padding-bottom here */}
+          <BusinessCard />
+        </div>
       </div>
     </div>
   );
 };
 
-export default Dog;
+export default Author;
